8f19fe74112dd1cf19a9b24836dc5e5d9da51e93
MajorM99 <majormike06@gmail.com>
2025-01-03 09:35:35 +0100

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -21,7 +21,7 @@ bot.addCommand(
     })
     const [date, time] = getDate()
     let CMD_HELP = `╭────────────────╮
-						ʟᴇᴠᴀɴᴛᴇʀ
+						Trøy's Bøt
 ╰────────────────╯
 
 ╭────────────────
@@ -90,7 +90,7 @@ bot.addCommand(
       }
     })
     const [date, time] = getDate()
-    let msg = `\`\`\`╭═══ LEVANTER ═══⊷
+    let msg = `\`\`\`╭═══ Trøy's Bøt ═══⊷
 ┃❃╭──────────────
 ┃❃│ Prefix : ${ctx.PREFIX}
 ┃❃│ User : ${message.pushName}
